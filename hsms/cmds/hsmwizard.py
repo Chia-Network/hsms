50dbebbbe7c2d07952cc9c5fa8699b333f307f91
Richard Kiss <him@richardkiss.com>
2022-04-27 15:28:40 -0700

    Handle device mounting.

--- hsms/cmds/hsmwizard.py
+++ hsms/cmds/hsmwizard.py
@@ -6,7 +6,6 @@ from pathlib import Path
 import argparse
 import os
 import secrets
-import sys
 import time
 
 import segno
@@ -16,11 +15,17 @@ from hsms.bls12_381 import BLSSecretExponent
 import hsms.cmds.hsms
 
 
-def wait_for_usb_mount(path):
-    print(f"insert USB stick for path {path}")
+def wait_for_usb_mount(parser, mount_path, device_to_mount, mount_command_read_only):
+    print(f"insert USB stick corresponding to {device_to_mount}")
     while 1:
-        if path.exists():
-            return
+        if device_to_mount.exists():
+            cmd = mount_command_read_only.format(
+                device_to_mount=device_to_mount, mount_path=mount_path
+            )
+            r = os.system(cmd)
+            if r == 0:
+                return
+            parser.exit(1, f"failed to mount device {device_to_mount} read-only")
         time.sleep(1)
 
 
@@ -63,6 +68,29 @@ def create_parser():
         description="Wizard to look for USB mount point and key file and launch hsms"
     )
     parser.add_argument(
+        "-d",
+        "--device-to-mount",
+        help="path, usually to something in `/dev/disk/` to ensure exists before attempting to mount",
+    )
+    parser.add_argument(
+        "-r",
+        "--mount-command-read-only",
+        help="command to mount disk read-only",
+        default="/usr/bin/sudo mount -o ro {device_to_mount} {mount_path}",
+    )
+    parser.add_argument(
+        "-w",
+        "--remount-command-read-write",
+        help="command to remount disk read-write",
+        default="/usr/bin/sudo mount -o remount,rw {device_to_mount} {mount_path}",
+    )
+    parser.add_argument(
+        "-u",
+        "--unmount-command",
+        help="command to unmount disk",
+        default="/usr/bin/sudo umount {mount_path}",
+    )
+    parser.add_argument(
         "path_to_secret_exponent_file",
         metavar="bech32m-encoded-se-file",
     )
@@ -73,12 +101,31 @@ def main():
     parser = create_parser()
     args = parser.parse_args()
     wallet_path = Path(args.path_to_secret_exponent_file)
-    wait_for_usb_mount(wallet_path.parent)
+    mount_path = wallet_path.parent
+    if args.device_to_mount:
+        wait_for_usb_mount(
+            parser, mount_path, Path(args.device_to_mount), args.mount_command_read_only
+        )
     if wallet_path.exists():
         parser = hsms.cmds.hsms.create_parser()
         args = parser.parse_args([str(wallet_path)])
         hsms.cmds.hsms.hsms(args, parser)
     else:
+        if args.remount_command_read_write:
+            cmd = args.remount_command_read_write.format(
+                device_to_mount=args.device_to_mount, mount_path=mount_path
+            )
+            r = os.system(cmd)
+            if r != 0:
+                parser.exit(
+                    1, f"failed to remount device {args.device_to_mount} read/write"
+                )
+            cmd = f"/usr/bin/touch {wallet_path}.tmp && /usr/bin/rm {wallet_path}.tmp"
+            r = os.system(cmd)
+            if r != 0:
+                parser.exit(
+                    1, f"could not create temporary file `{wallet_path}.tmp`, drive permissions may be wrong"
+                )
         generate_secret(wallet_path)
     print()
     input("hit return to power off> ")
